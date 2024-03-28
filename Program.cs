4715346a5d44548e472f785dfe5026d3376f1dde
Rafael Bisol <rafaelpbisol@hotmail.com>
2024-03-28 18:35:08 -0300

    Projeto criado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BancoOO_aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
