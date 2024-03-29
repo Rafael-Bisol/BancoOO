2a0c1e7f0d3cc56ff82db27e30b9240b6cb3678c
Rafael Bisol <rafaelpbisol@hotmail.com>
2024-03-28 21:14:48 -0300

    correções de formatação

--- Program.cs
+++ Program.cs
@@ -9,11 +9,11 @@ namespace BancoOO_aula
         {
             Console.WriteLine("Olá senhor!\nBem-vindo ao Banco FDP!");
             
-            Console.Write("Para criar sua conta primeiro conte-nos seu nome\n> ");
+            Console.Write("Para criar sua conta primeiro conte-nos seu nome:\n> ");
             // Conta conta1 = new Conta(Console.ReadLine());
             string nome = Console.ReadLine();
 
-            Console.Write("Digite o número inicial da conta\n> ");
+            Console.Write("Digite o número inicial da conta:\n> ");
             int numConta = int.Parse(Console.ReadLine());
 
             Console.Write("Deseja fazer um depósito inicial? (S/N)\n> ");
@@ -23,19 +23,19 @@ namespace BancoOO_aula
 
             if (resposta.StartsWith('S') || resposta.StartsWith('s'))
             {
-                Console.Write("Digite o valor do deposito inicial\n> ");
+                Console.Write("Digite o valor do deposito inicial:\n> ");
                 deposito = double.Parse(Console.ReadLine());
             }
             Conta conta1 = new Conta(nome, deposito, numConta);
-            Console.WriteLine($"Seu saldo é: {conta1.Saldo:c}\n");
+            Console.WriteLine($"Seu saldo é: {conta1.Saldo:c}");
 
-            Console.Write("Por favor, faça um depósito\n> ");
+            Console.Write("Por favor, faça um depósito:\n> ");
             deposito = double.Parse(Console.ReadLine());
             conta1.Deposito(deposito);
 
             Console.WriteLine($"Seu saldo é {conta1.Saldo:c}");
             
-            Console.Write("Faça um saque (atente-se: há uma taxa de R$5,00 por saque)\n> ");
+            Console.Write("Faça um saque (atente-se: há uma taxa de R$5,00 por saque):\n> ");
             conta1.Saque(double.Parse(Console.ReadLine()));
 
             Console.WriteLine($"Seu saldo atualizado é {conta1.Saldo:c}");
